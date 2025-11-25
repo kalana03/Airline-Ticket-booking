ba32e492c472df03c8d14d15ace0c3c60703ba66
Kalana Abeysundara <knbabeysundara2003.it@gmail.com>
2025-11-25 22:18:30 +0530

    Initial commit - Airline Ticket Booking System

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import heroImg from "./assets/Images/hero.webp"; // adjust path to your image
+import FlightSearchForm from "./form.jsx";
+
+function App() {
+  return (
+    <div className="w-full h-screen relative">
+      {/* Hero section */}
+      <div
+        className="w-full h-full bg-center bg-cover  flex flex-col items-center justify-between"
+        style={{ backgroundImage: `url(${heroImg})`, objectFit: "cover" }}
+      >
+        <div className="text-center bg-opacity-40 p-8 rounded-lg flex flex-col gap-6">
+          <h1 className="text-5xl md:text-6xl font-bold text-blue mt-[25%] ">
+            Welcome to Delta Airlines
+          </h1>
+          <p className="text-lg md:text-xl text-green mb-6">
+            Book your flights easily and conveniently with Delta Airlines.
+          </p>
+        </div>
+        <FlightSearchForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './index.css'
+import './App.css'
+
+function Header() {
+  return (
+    <header className="bg-white text-primary-content shadow-md fixed top-0 left-0 w-full z-10">
+      <div className=" mx-8 my-4 flex justify-between items-center px-2">
+        {/* Logo */}
+        <div className="flex items-center space-x-4">
+          <img
+            src="src/assets/Images/Delta-Air-Lines-Logo.png"
+
+            className="h-8 rounded-full"
+          />
+
+        </div>
+
+        {/* Navigation */}
+        <nav className="space-x-4 flex gap-12">
+
+          <a href="#" className="btn btn-ghost btn-sm">
+            Flights
+          </a>
+          <a href="#" className="btn btn-ghost btn-sm">
+            About
+          </a>
+          <a href="#" className="btn btn-ghost btn-sm">
+            Contact
+          </a>
+        </nav>
+
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
